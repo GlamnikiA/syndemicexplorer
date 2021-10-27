136c218604285ade335992e86a418bd6489c1612
Manna-Manojlovic <manojlovic.manna@gmail.com>
2021-10-27 16:23:07 +0200

    creates endpoints for area codes 1,2,3 from syndemicdb

---
+++ src/initsql/02_socioeconomiq.sql
@@ -0,0 +1,3 @@
+\connect syndemic
+
+CREATE TABLE demographics (source TEXT, year INTEGER, country_code TEXT, area1_code TEXT, area2_code TEXT, area3_code TEXT, Gid TEXT, indicator TEXT, sample_size INTEGER, value INTEGER);
